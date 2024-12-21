7a5633ca09de96129d630ad979029c1b9e8c2582
NahuelEF <nahuelfedele@gmail.com>
2024-12-20 22:59:45 -0300

    add: custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
+    colors: {
+      transparent: "transparent",
+      black: "hsl(0, 0, 0%)",
+      green: "hsl(163, 72%, 41%)", // Lime Green
+      red: "hsl(356, 69%, 56%)", // Bright Red
+      "facebook-blue": "hsl(208, 92%, 53%)", // Facebook Blue
+      "twitter-blue": "hsl(203, 89%, 53%)", // Twitter Blue
+      "instagram-yellow": "hsl(37, 97%, 70%)", // Instagram Gradient Start
+      "instagram-pink": "hsl(329, 70%, 58%)", // Instagram Gradient End
+      "youtube-red": "hsl(348, 97%, 39%)", // YouTube Red
+
+      teal: "hsl(210, 78%, 56%)", // Toggle Gradient Start
+      emerald: "hsl(146, 68%, 55%)", // Toggle Gradient End
+      gray: "hsl(230, 22%, 74%)", // Toggle Light Theme
+
+      charcoal: "hsl(230, 17%, 14%)", // Very Dark Blue (BG and Text)
+      navy: "hsl(232, 19%, 15%)", // Very Dark Blue (Top BG Pattern)
+      "dark-blue": "hsl(228, 28%, 20%)", // Dark Desaturated Blue (Card BG)
+      "muted-blue": "hsl(228, 34%, 66%)", // Desaturated Blue (Text)
+
+      "pale-blue": "hsl(225, 100%, 98%)", // Very Pale Blue (Top BG Pattern)
+      "light-blue": "hsl(227, 47%, 96%)", // Light Grayish Blue (Card BG)
+      "subdued-blue": "hsl(228, 12%, 44%)", // Dark Grayish Blue (Text)
+      white: "hsl(0, 0%, 100%)", // (BG and Text)
+    },
     container: {
       center: true,
       padding: "2rem",
@@ -34,4 +60,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
